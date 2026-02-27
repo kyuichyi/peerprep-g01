3af8b63aa2ae4dcb42399eb2bbf644a1b5534874
kyuichyi <chongkaitlyn@u.nus.edu>
2026-02-28 01:33:44 +0800

    amended endpt paths

--- user-service/src/routes/authRoutes.js
+++ user-service/src/routes/authRoutes.js
@@ -3,15 +3,15 @@ const router = express.Router()
 const { register, login, logout } = require('../controllers/authController')
 
 // call the register function
-// when a POST request is made to http://localhost:8080/api/auth/register
+// when a POST request is made to http://localhost:3001/api/auth/register
 router.route('/register').post(register)
 
 // call the login function
-// when a POST request is made to http://localhost:8080/api/auth/login
+// when a POST request is made to http://localhost:3001/api/auth/login
 router.route('/login').post(login)
 
 // call the logout function
-// when a POST request is made to http://localhost:8080/api/auth/logout
+// when a POST request is made to http://localhost:3001/api/auth/logout
 router.route('/logout').post(logout)
 
 module.exports = router
