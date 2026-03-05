61a6d2d7b13051ef535a97b00e0941c09a6bd6c0
kyuichyi <chongkaitlyn@u.nus.edu>
2026-03-05 11:34:47 +0800

    feat: create function to add topic

---
+++ questionBank-service/src/controllers/topicController.js
@@ -0,0 +1,47 @@
+const db = require('../config/db');
+
+/**
+ * GET /api/questions/topics
+ * 
+ * Returns all available topic names from the "topic" table.
+ */
+exports.getAllTopics = async (req, res) => {
+    try {
+
+    } catch (error) {
+        console.error('Error fetching topics:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }   
+};
+
+/**
+ * POST /api/questions/topics
+ * 
+ * Body: { name }
+ *
+ * Adds a new topics to the "topic" table.
+ * Ensures the topic name is unique.
+ */
+exports.addTopic = async (req, res) => {
+  const { topicName } = req.body;
+
+  try {
+    const [result] = await db.execute('INSERT INTO topic (name) VALUES (?)', [topicName]);
+    res.status(201).json({ 
+      message: 'Topic added successfully',
+      id: result.insertId, 
+      name: topicName });
+  } catch (error) {
+    if (error.code === 'ER_DUP_ENTRY') {
+      return res.status(400).json({ error: 'Topic name already exists' });
+    }
+
+    console.error('Error adding topic:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+module.exports = {
+  getAllTopics,
+  addTopic
+};
