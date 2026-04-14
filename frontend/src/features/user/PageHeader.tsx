0ffa1cdced5a407488a072461927301894521b14
findingfaey <yifeiyangyang@gmail.com>
2026-04-14 23:58:03 +0800

    feat: add logout button to PageHeader and AdminSideMenu

--- frontend/src/features/user/PageHeader.tsx
+++ frontend/src/features/user/PageHeader.tsx
@@ -1,8 +1,18 @@
-import { Box, useTheme } from "@mui/material";
+import { Box, IconButton, useTheme } from "@mui/material";
+import LogoutIcon from "@mui/icons-material/Logout";
 import logo from "../../assets/peerprep-logo-nobg.png";
+import useAuth from "../../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 
 function PageHeader() {
   const theme = useTheme();
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    logout();
+    navigate("/login");
+  }
 
   return (
     <Box
@@ -10,6 +20,7 @@ function PageHeader() {
         padding: 0,
         display: "flex",
         alignItems: "center",
+        justifyContent: "space-between",
         bgcolor: "white",
         borderBottom: "1px solid rgba(0, 0, 0, 0.2)",
       }}
@@ -24,6 +35,9 @@ function PageHeader() {
           padding: theme.spacing(0.8, 1, 0.6, 1),
         }}
       />
+      <IconButton onClick={handleLogout} sx={{ mr: 1 }} title="Logout">
+        <LogoutIcon />
+      </IconButton>
     </Box>
   );
 }
